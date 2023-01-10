2bf95ecb1dc522a85de5f1f88856158463cc6356
twj2 <brianteh007@gmail.com>
2023-01-10 08:20:09 +0800

    added fetch functions

--- game_1/scenes/testgame_scene.js
+++ game_1/scenes/testgame_scene.js
@@ -1,16 +1,23 @@
 import { Scene } from 'phaser';
 
+//import functions here from /scripts
+import {foo1} from '../scripts/fetch_functions'
+import {foo2} from '../scripts/fetch_functions';
+
 export default class testgame_scene extends Scene{
     constructor(){
         super('testgame');
     }
     preload(){
+        foo1("haha")
+        foo2()
         this.load.image('animal1','animations/cow.png');
         this.load.image('background_1', 'backgrounds/background_1.png');
     }
     create(){
         this.add.image(400,300,'animal1');
         this.add.image(0,0,'background_1');
+
     }
     update(){
 
--- game_1/scripts/load_all_scenes.ts
+++ game_1/scripts/load_all_scenes.ts
@@ -1,8 +1,8 @@
 
 async function loadAllScenes(){
     //import scenes here
-    const {default :preloader_scene} = await import("../scenes/preloader_scene")
-    const {default :testgame_scene} = await import("../scenes/testgame_scene")
+    const {default :preloader_scene} = await import("../scenes/preloader_scene.js")
+    const {default :testgame_scene} = await import("../scenes/testgame_scene.js")
 
     const scenes = [
         preloader_scene,
