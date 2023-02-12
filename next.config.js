7282fd12cb197e4137a84eacc7e00dc3760ebc9b
twj2 <brianteh007@gmail.com>
2023-02-12 21:21:14 +0800

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  // env:{
-  //   infura_api_key:"b94db70b002d45a4a2ca2c504a535242",
-  //   alchemy_api_key:"I9xOBTLDICaKQ4sRu4eXcoZuuCeAxAzr",
-  //   socket_api_url:"https://mutable-nft-api.onrender.com"
-  // }
+  reactStrictMode: false
 }
 
 module.exports = nextConfig
